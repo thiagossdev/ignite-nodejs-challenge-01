9d43d70152d4186264f5fbaed4d47ae17368537e
Thiago S. Santos <ssthiago.dev@gmail.com>
2022-11-15 19:37:32 -0300

    Add mark todo as done

--- src/index.js
+++ src/index.js
@@ -93,7 +93,20 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = findTodo(user, id);
+
+  if (!todo) {
+    return response.status(404).json({
+      error: 'TODO not found!',
+    });
+  }
+
+  todo.done = true;
+
+  return response.status(201).json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
