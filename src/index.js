8b8650ac5f27ecd926478bc1a4f24b66d35c2c56
Thiago S. Santos <ssthiago.dev@gmail.com>
2022-11-15 19:35:30 -0300

    Add create user

--- src/index.js
+++ src/index.js
@@ -1,21 +1,50 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+  request.user = user;
+
+  if (!user) {
+    return response.status(400).json({
+      error: 'User not found!',
+    });
+  }
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { username, name } = request.body;
+
+  const userAlreadyExists = users.some((user) => user.username === username);
+
+  if (userAlreadyExists) {
+    return response.status(400).json({
+      error: 'User already exists!',
+    });
+  }
+
+  const user = {
+    id: uuidv4(),
+    username,
+    name,
+    todos: [],
+  };
+
+  users.push(user);
+
+  return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
